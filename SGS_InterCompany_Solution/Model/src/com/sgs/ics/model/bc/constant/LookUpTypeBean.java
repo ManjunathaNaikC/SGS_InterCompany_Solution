1c671f45fba5fa6b7827bba74e71e310fa04da31
8vikranth1989 <109280250+8vikranth1989@users.noreply.github.com>
2022-11-28 17:51:07 +0530

    cost Identification Rule page

---
+++ SGS_InterCompany_Solution/Model/src/com/sgs/ics/model/bc/constant/LookUpTypeBean.java
@@ -0,0 +1,12 @@
+package com.sgs.ics.model.bc.constant;
+
+public class LookUpTypeBean {
+    public LookUpTypeBean() {
+        super();
+    }
+   public  static final String ALLOCATION_BASIS="ALLOCATION_BASIS";
+   public  static final String DATA_SOURCE="DATASOURCE_TYPE";
+   public  static final String ACCOUNTING_TREATMENT="ACC_TREAT";  
+   public  static final String SERVICE_CATEGORY="SERVICE_CATEGORY";
+   public  static final String NATURE_OF_EXPENSE="NATURE_OF_EXPENSE";
+}

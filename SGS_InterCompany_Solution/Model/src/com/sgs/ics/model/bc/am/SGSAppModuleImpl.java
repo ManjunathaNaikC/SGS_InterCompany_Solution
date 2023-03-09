592dae7859f3e92403758f33592fff470d05405f
rajapeddanna <119409742+rajapeddanna@users.noreply.github.com>
2023-03-09 17:38:23 +0530

    Credit Memo Screen Changes

--- SGS_InterCompany_Solution/Model/src/com/sgs/ics/model/bc/am/SGSAppModuleImpl.java
+++ SGS_InterCompany_Solution/Model/src/com/sgs/ics/model/bc/am/SGSAppModuleImpl.java
@@ -67,7 +67,7 @@ public class SGSAppModuleImpl extends ApplicationModuleImpl implements SGSAppMod
             Connection conn = null;
         try {
 
-          String connectionUrl = "jdbc:sqlserver://localhost;instanceName=SQLEXPRESS;databasename=SGS_NEW;integratedSecurity=true;";
+          String connectionUrl = "jdbc:sqlserver://localhost;instanceName=MSSQLSERVER;databasename=SGSICO;integratedSecurity=true;";
           conn = DriverManager.getConnection(connectionUrl);
  //           conn = DriverManager.getConnection("jdbc:sqlserver://ASBCOLPS02:1433;databaseName=DEVINTER","EYUser","Ey@123");
        
@@ -742,6 +742,22 @@ public class SGSAppModuleImpl extends ApplicationModuleImpl implements SGSAppMod
      */
     public ViewLinkImpl getSgsStdRateDocVL1() {
         return (ViewLinkImpl) findViewLink("SgsStdRateDocVL1");
+    }
+
+    /**
+     * Container's getter for SgsInvoiceCreditMemoVO1.
+     * @return SgsInvoiceCreditMemoVO1
+     */
+    public ViewObjectImpl getSgsInvoiceCreditMemoVO1() {
+        return (ViewObjectImpl) findViewObject("SgsInvoiceCreditMemoVO1");
+    }
+
+    /**
+     * Container's getter for CreditMemoLOVVO1.
+     * @return CreditMemoLOVVO1
+     */
+    public ViewObjectImpl getCreditMemoLOVVO1() {
+        return (ViewObjectImpl) findViewObject("CreditMemoLOVVO1");
     }
 }
 

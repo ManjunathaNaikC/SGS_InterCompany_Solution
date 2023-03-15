2f243200c697cf90062118c794d531a5e8835ce3
krnkumarEY <m.kumar@in.ey.com>
2023-03-15 15:33:02 +0530

    Adj Entries Upload

--- SGS_InterCompany_Solution/Model/src/com/sgs/ics/model/bc/am/SGSAppModuleImpl.java
+++ SGS_InterCompany_Solution/Model/src/com/sgs/ics/model/bc/am/SGSAppModuleImpl.java
@@ -67,7 +67,7 @@ public class SGSAppModuleImpl extends ApplicationModuleImpl implements SGSAppMod
             Connection conn = null;
         try {
 
-          String connectionUrl = "jdbc:sqlserver://localhost;instanceName=MSSQLSERVER;databasename=SGSICO;integratedSecurity=true;";
+          String connectionUrl = "jdbc:sqlserver://localhost;instanceName=SQLEXPRESS;databasename=SGS_New;integratedSecurity=true;";
           conn = DriverManager.getConnection(connectionUrl);
  //           conn = DriverManager.getConnection("jdbc:sqlserver://ASBCOLPS02:1433;databaseName=DEVINTER","EYUser","Ey@123");
        
@@ -765,6 +765,22 @@ public class SGSAppModuleImpl extends ApplicationModuleImpl implements SGSAppMod
      */
     public ViewObjectImpl getCreditMemoLOVVO1() {
         return (ViewObjectImpl) findViewObject("CreditMemoLOVVO1");
+    }
+
+    /**
+     * Container's getter for TxnCategoryLOVVO1.
+     * @return TxnCategoryLOVVO1
+     */
+    public ViewObjectImpl getTxnCategoryLOVVO1() {
+        return (ViewObjectImpl) findViewObject("TxnCategoryLOVVO1");
+    }
+
+    /**
+     * Container's getter for YesOrNoLookupVO1.
+     * @return YesOrNoLookupVO1
+     */
+    public ViewObjectImpl getYesOrNoLookupVO1() {
+        return (ViewObjectImpl) findViewObject("YesOrNoLookupVO1");
     }
 }
 

6c8faa608b009665e68c42af9704f381e68dbc83
rajapeddanna <119409742+rajapeddanna@users.noreply.github.com>
2023-03-16 13:56:07 +0530

    Contra changes in create settlement

--- SGS_InterCompany_Solution/Model/src/com/sgs/ics/model/bc/am/SGSAppModuleImpl.java
+++ SGS_InterCompany_Solution/Model/src/com/sgs/ics/model/bc/am/SGSAppModuleImpl.java
@@ -67,7 +67,7 @@ public class SGSAppModuleImpl extends ApplicationModuleImpl implements SGSAppMod
             Connection conn = null;
         try {
 
-          String connectionUrl = "jdbc:sqlserver://localhost;instanceName=MSSQLSERVER;databasename=SGSICO;integratedSecurity=true;";
+          String connectionUrl = "jdbc:sqlserver://localhost;instanceName=MSSQLSERVER;databasename=DEVINTER;integratedSecurity=true;";
           conn = DriverManager.getConnection(connectionUrl);
  //           conn = DriverManager.getConnection("jdbc:sqlserver://ASBCOLPS02:1433;databaseName=DEVINTER","EYUser","Ey@123");
        
